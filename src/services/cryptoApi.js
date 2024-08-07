153220f540f2aff6c176385b71835634a529b173
Hasan-Irfan <riyankam82@gmail.com>
2024-08-07 04:13:49 -0700

    updated

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
 import { createApi , fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-    'x-rapidapi-key': 'coinranking1dc32fdf06bd0e31c5f85200a52920fd02604bd9b60e203c',
+    'x-rapidapi-key': process.env.REACT_APP_COIN_RANKING_API_KEY,
     'x-rapidapi-host': 'api.coinranking.com/v2'
 }
 
@@ -31,23 +31,3 @@ export const {
     useGetCoinHistoryQuery,
 } = cryptoApi;
 
-
-
-// const options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/stats',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl'
-//     },
-//     headers: {
-//       'x-rapidapi-key': '8fe0d4f1ecmsh7bd4fd0dc7f3a78p19539cjsn13167edd4ddd',
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
-//     }
-//   };
-  
-//   try {
-//       const response = await axios.request(options);
-//       console.log(response.data);
-//   } catch (error) {
-//       console.error(error);
-//   }
